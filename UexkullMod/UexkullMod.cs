499bca16a19b47c08a32c9a8137c33700bc224e0
Yiansie <guillermocg10@gmail.com>
2021-03-16 15:56:34 +0100

    Add files via upload

---
+++ UexkullMod/UexkullMod.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+
+namespace UexkullMod
+{
+	public class UexkullMod : Mod
+	{
+		public UexkullMod()
+        {
+
+        }
+	}
+}
---
+++ UexkullMod/UxModPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace UexkullMod
+{
+    class UxModPlayer : ModPlayer
+    {
+        
+        public bool uexkullPet;
+
+        public override void ResetEffects()                 //Resetea todas las variables a default
+        {
+            uexkullPet = false;
+        }
+    }
+}
